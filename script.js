eb52257f807f29e227e724b239472e6a0a133939
Zaheer Ahmed <ranazaheerahmed65@gmail.com>
2024-07-05 13:52:33 +0500

    update

--- script.js
+++ script.js
@@ -73,19 +73,13 @@ function myFunction() {
   }
 }
 var navContainer = document.getElementById("navbar");
-
-// Get all buttons with class="tabs" inside the container
 var tabs = navContainer.getElementsByClassName("tabs");
-
-// Loop through the buttons and add the active class to the current/clicked button
 for (var i = 0; i < tabs.length; i++) {
     tabs[i].addEventListener("click", function() {
-        // Remove the active class from the current active tab
         var current = document.getElementsByClassName("active");
         if (current.length > 0) {
             current[0].className = current[0].className.replace(" active", "");
         }
-        // Add the active class to the clicked tab
         this.className += " active";
     });
 }
